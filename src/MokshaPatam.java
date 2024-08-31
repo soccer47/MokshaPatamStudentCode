4051c698419a860f047c17ee9249ebcace2e3b38
soccer47 <stevie.halprin@menloschool.org>
2024-08-30 19:43:14 -0700

    2nd work period

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -16,36 +16,78 @@ public class MokshaPatam {
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
 
-        int[][] visitedSq = new int[boardsize][boardsize];
+        // Initialize
+        boolean[][] visitedSq = new boolean[boardsize][boardsize];
+        // Lowest number of turns taken starts at
+        int lowestTurns = Integer.MAX_VALUE;
 
-
-
-        return 0;
+        for (int i = 1; i < 7; i++) {
+            int turns = rollResult(1, i, ladders, snakes, visitedSq);
+            if (turns == -1 || turns > lowestTurns) {
+                continue;
+            }
+            lowestTurns = turns;
+        }
+        return lowestTurns;
     }
 
-    public static int[][] rollResult(int oldSquare, int roll, int[][] ladders, int[][] snakes, int[][] visitedSq) {
+    public static int rollResult(int oldSquare, int roll, int[][] ladders, int[][] snakes, boolean[][] visitedSq) {
         // If the current square is the end square or has already been visited, return the visited squares
-        if (oldSquare == visitedSq.length * visitedSq.length || oldSquare == visitedSq[oldSquare / 10][oldSquare % 10]) {
-            return visitedSq;
+        if (oldSquare == visitedSq.length * visitedSq.length || visitedSq[oldSquare / 10][oldSquare % 10] == true) {
+            return countTurns(visitedSq);
         }
 
         // Change the value of the current square in visitedSq from 0 to 1
         // This shows that the square has now been visited
-        visitedSq[oldSquare / 10][oldSquare % 10] = 1;
+        visitedSq[oldSquare / 10][oldSquare % 10] = true;
 
         int resultSquare = oldSquare + roll;
+        int lowestTurns = Integer.MAX_VALUE;
+        boolean[][] lowestPath = new boolean[visitedSq.length][visitedSq.length];
 
         for (int i = 0; i < ladders.length; i++) {
             if (resultSquare == ladders[i][1]) {
-                return rollResult(ladders[i][2], 1, ladders, snakes, visitedSq);
+                resultSquare = ladders[i][2];
             }
         }
         for (int i = 0; i < snakes.length; i++) {
             if (resultSquare == snakes[i][1]) {
-                return rollResult(snakes[i][2], 1, ladders, snakes, visitedSq);
+                resultSquare = snakes[i][2];
             }
         }
 
-        return rollResult(resultSquare, 1, ladders, snakes, visitedSq);
+        for (int i = 1; i < 7; i++) {
+            int turns = rollResult(resultSquare, i, ladders, snakes, visitedSq);
+            if (turns == -1 || turns > lowestTurns) {
+                continue;
+            }
+            lowestTurns = turns;
+        }
+        return lowestTurns;
     }
+
+    // Method counts the number of turns taken by counting how many squares have been visited in boolean array
+    public static int countTurns(boolean[][] visitedSquares) {
+
+        // If the end square hasn't been reached, return -1
+        if (visitedSquares[visitedSquares.length][visitedSquares.length] == false) {
+            return -1;
+        }
+
+        // Number of turns taken initialized to 0
+        int turns = 0;
+        // Traverses through every square in boolean array visitedSquares
+        for (int j = 0; j < visitedSquares.length; j++) {
+            for (int k = 0; k < visitedSquares.length; k++) {
+                // If square has been visited, increase turns by 1
+                if (visitedSquares[j][k] == true) {
+                    turns++;
+                }
+            }
+        }
+
+        // Returns the number of turns taken
+        return turns;
+    }
+
 }
